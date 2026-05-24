0523b58d152f38efbc2295e2de1bba9e2e5be2f8
root <root@v3128495.hosted-by-vdsina.ru>
2026-05-24 20:16:58 +0300

    add workers for uvicorn and pool settings for db

--- app/db/database.py
+++ app/db/database.py
@@ -12,6 +12,11 @@ engine = create_async_engine(
     settings.database_url,
     echo=False,
     future=True,
+
+    pool_size=20,
+    max_overflow=30,
+    pool_timeout=30,
+    pool_recycle=1800,
 )
 
 AsyncSessionLocal = async_sessionmaker(
